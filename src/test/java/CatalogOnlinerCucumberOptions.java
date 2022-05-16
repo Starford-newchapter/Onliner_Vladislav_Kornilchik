dd450393fd4515b40c033cf4d0e7a88e642fcf52
ua.karnilchyk <v.kornilchik2015@gmail.com>
2022-05-16 16:57:14 +0300

    add plugin json

--- src/test/java/CatalogOnlinerCucumberOptions.java
+++ src/test/java/CatalogOnlinerCucumberOptions.java
@@ -2,7 +2,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = {"src/test/resources/features"}, glue = {"src/test/java/steps"})
+@CucumberOptions(features = {"src/test/resources/features"},
+        glue = {"src/test/java/steps"},
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
+
 public class CatalogOnlinerCucumberOptions extends AbstractTestNGCucumberTests {
 
     @Override
